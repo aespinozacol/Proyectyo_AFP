8109bd55d21a6449a425f8706bc9c25818df8c7b
Adolfo Espinoza <ivanadolfo16@gmail.com>
2022-06-03 15:21:32 -0500

    Primera actualizacion

--- src/main/java/com/afp/Proyectyo_AFP/service/implement/RegistrationRequestServiceImpl.java
+++ src/main/java/com/afp/Proyectyo_AFP/service/implement/RegistrationRequestServiceImpl.java
@@ -1,5 +1,6 @@
 package com.afp.Proyectyo_AFP.service.implement;
 
+import com.afp.Proyectyo_AFP.model.AFPAssociation;
 import com.afp.Proyectyo_AFP.model.RegistrationRequest;
 import com.afp.Proyectyo_AFP.repository.AFPAssociationRepository;
 import com.afp.Proyectyo_AFP.repository.RegistrationRequestRepository;
@@ -8,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class RegistrationRequestServiceImpl implements RegistrationRequestService {
@@ -22,21 +24,22 @@ public class RegistrationRequestServiceImpl implements RegistrationRequestServic
 
     @Override
     public RegistrationRequest create(RegistrationRequest registrationRequest) {
-        return null;
+        return registrationRequestRepository.save(registrationRequest);
     }
 
     @Override
     public RegistrationRequest update(RegistrationRequest registrationRequest) {
-        return null;
+        return registrationRequestRepository.save(registrationRequest);
     }
 
     @Override
     public RegistrationRequest findById(long id) {
-        return null;
+        Optional<RegistrationRequest> op = registrationRequestRepository.findById(id);
+        return op.isPresent() ? op.get() : new RegistrationRequest();
     }
 
     @Override
     public void delete(long id) {
-
+        registrationRequestRepository.deleteById(id);
     }
 }
